e8890139b84ffb948311abca4504ec6287a82b47
Hendrix <afamorim16@sapo.pt>
2024-05-06 21:47:03 +0100

    Organização e dependências

--- tpEW/app.js
+++ tpEW/app.js
@@ -2,9 +2,36 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var mongoose = require('mongoose')
+
+var mongoDB = 'mongodb://127.0.0.1:27017/tpMapaRuasDeBraga'
+mongoose.connect(mongoDB)
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'Erro de conexão ao MongoDB'))
+db.once('open', () => {
+  console.log("Conexão ao MongoDB realizada com sucesso")
+})
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var ucRouter = require('./routes/uc');
+var equipaRouter = require('./routes/equipa');
+var projetoRouter = require('./routes/projeto');
+var entregaRouter = require('./routes/entrega');
+var userRouter = require('./routes/user');
+
+const { v4: uuidv4 } = require('uuid'); 
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy
+var axios = require('axios')
+
+// passport config
+var User = require('./models/user');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 var app = express();
 
@@ -17,8 +44,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  genid: req => {
+    console.log("Dentro do Middleware da sessão...");
+    console.log(req.sessionID);
+    return uuidv4();
+  },
+  store: new FileStore(), // caso queiramos guardar os cookies localmente
+  secret: "TPMapaRuasDeBragaEngWeb2024",
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/uc', ucRouter);
+app.use('/equipa', equipaRouter);
+app.use('/projeto', projetoRouter);
+app.use('/entrega', entregaRouter);
+app.use('/user', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ tpEW/auth/auth.js
@@ -0,0 +1,20 @@
+var jwt = require('jsonwebtoken')
+
+module.exports.verificaAcesso = function (req, res, next){
+    var myToken = req.query.token || req.body.token
+    if(myToken){
+      jwt.verify(myToken, "TPMapaRuasDeBragaEngWeb2024", function(e, payload){
+        if(e){
+          res.status(401).jsonp({error: e})
+        }
+        else{
+          console.log(payload)
+          next()
+        }
+      })
+    }
+    else{
+      res.status(401).jsonp({error: "Token inexistente!"})
+    }
+  }
+

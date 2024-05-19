9ce57a379053eb201a2f145b76895456063074e0
Hendrix <afamorim16@sapo.pt>
2024-05-19 03:13:22 +0100

    Update ruas.js

--- tpEW/APIdeDados/routes/ruas.js
+++ tpEW/APIdeDados/routes/ruas.js
@@ -4,9 +4,29 @@ var Rua = require('../controllers/rua')
 var fs = require('fs')
 
 router.get('/', function(req, res) {
+  if(req.query.nome){
+    const nomeRua = req.query.nome
+    Rua.findRuaByNome(nomeRua)
+    .then(data => res.jsonp(data))
+    .catch(erro => res.jsonp(erro))
+  }
+  else if(req.query.data){
+    const dataR = req.query.data
+    Rua.listaRuasByData(dataR)
+    .then(data => res.jsonp(data))
+    .catch(erro => res.jsonp(erro))
+  }
+  else if(req.query.lugar){
+    const lugarRua = req.query.lugar
+    Rua.listaRuasByLugar(lugarRua)
+    .then(data => res.jsonp(data))
+    .catch(erro => res.jsonp(erro))
+  }
+  else{
     Rua.list()
     .then(data => res.jsonp(data))
     .catch(erro => res.jsonp(erro))
+  }
 });
 
 router.get('/:id', function(req, res) {

d8c04081a9c072faf991357c0f07e334834f9c3d
hugomes21 <a96842@alunos.uminho.pt>
2024-05-31 16:31:47 +0100

    resolver o acesso à info do dataset

--- tpEW/Interface/routes/index.js
+++ tpEW/Interface/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('API de Dados - TP Engenharia Web');
+  var date = new Date().toISOString().substring(0, 16);
+  axios.get('http://localhost:1893/ruas/')
+  .then(resp => {
+    var ruas = resp.data;
+    res.status(200).render('index', { "Ruas": ruas, "Data": date });
+  })
+  .catch(error => {
+    res.status(500).render('error', { "error": error });
+  });
 });
 
 module.exports = router;

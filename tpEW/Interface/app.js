12f12d8d46641f8cd463c2aa8e0a03fede5f912a
MatildeF25 <a95319@alunos.uminho.pt>
2024-06-03 23:03:20 +0100

    nada

--- tpEW/Interface/app.js
+++ tpEW/Interface/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 
 var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -22,51 +22,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({
-  genid: req => {
-    console.log("Dentro do Middleware da sessão");
-    console.log(req.sessionID);
-    return uuidv4();
-  },
-  store: new FileStore(),
-  secret: "O meu segredo",
-  resave: false,
-  saveUninitialized: true
-}));
-
-app.use(passport.initialize())
-app.use(passport.session())
-
-passport.use(new LocalStrategy(
-  {usernameField: 'username'}, (username, password, done) =>{
-    axios.get("http://localhost:1893/ruas?id=" + username)
-      .then(dados => {
-        const user = dados.data[0]
-        if(!user) { return done(null, false, {message: "Utilizador inexistente: " + username})}
-        if(password != user.password) { return done(null, false, {message: "Password inválida."})}
-      })
-      .catch(erro => done(erro))
-  }
-))
-
-// Indica-se ao passport como serializar o utilizador
-passport.serializeUser ((user, done) => {
-  console.log("Vou serializar o user na sessão: " + JSON.stringify(user))
-  // Serialização do utilizador. O passport grava o utilizador na sessão aqui.
-  done(null, user.id)
-})
-
-// Desserialização: a partir do id obtem-se a informação do utilizador
-passport.deserializeUser((uid, done) => {
-  console.log("Vou desserializar o user: " + uid)
-  axios.get("http://localhost:5011/users/" + uid)
-    .then(dados => done(null, dados.data))
-    .catch(erro => done(erro, false))
-})
-
 
 app.use('/', indexRouter);
-//app.use('/users', usersRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
